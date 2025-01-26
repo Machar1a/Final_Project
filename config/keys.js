ee466f0151a58f193362105409c8754c0efbd0bd
Machar1a <arnoldmarch7@gmail.com>
2025-01-26 14:28:40 +0300

    Config Module with db access added, auth module and strategy defined - {Config dir}

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://macharia:'+ encodeURIComponent('access1$3&') + '@tetsu.8gewr.mongodb.net/test?retryWrites=true&w=majority&appName=Tetsu';
+
+module.exports = {
+    mongoURI: dbPassword
+};
